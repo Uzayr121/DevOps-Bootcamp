1560859cfc01f60c21340f5615ea5d8cb65d24ff
uzayr121 <uzayr121.uk@gmail.com>
2025-02-09 22:38:31 +0000

    changed script to make logging disk usage check more efficient

--- Bash/scripts/disk-usage.sh
+++ Bash/scripts/disk-usage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+Directory=$1 # this is the directory that the logs will be saved to
 diskspace=$(df -h ~/ | awk '{print $5}' | tail -n +2 | tr -d %) # this check disk space of the home directory, only prints the percentage section, only shows the number, and takes out the % sign so we can use it in the if statement
 TS=$(date "+%Y-%m-%d %H-%M-%S") # we use - in the timestamp so there are no errors with the filename
 file="disk_usage_$TS.log" # this is the file that the logs will be saved to
@@ -12,7 +12,7 @@ while true; do # use a while loop because we want to check contiuously
                 echo "disk space is below 85%"
         fi
         echo "logging check to : $file"
-         echo "this is the current diskspace of the home directory "$diskspace"" | sudo tee -a /var/log/"$file" > /dev/null # appends to the file of your choice, we use the append operator because we want each check to be logged instead of each one overwriting each other
+         echo "this is the current diskspace of the home directory "$diskspace"" >> "$Directory"/"$file" # appends to the file of your choice, we use the append operator because we want each check to be logged instead of each one overwriting each other
         # we have to use sudo otherwise it says permission denied, we use tee -a to append to the file of our choice, we have to use this otherwise nothing happens because there is no command after sudo so it doesn't take the input from echo into the file
         # we direct it to /dev/null because tee will take the input, write it to the file and to stdout. So we use it so that it doesn't print our message in the terminal
         sleep 600 # checks every 10 minutes
